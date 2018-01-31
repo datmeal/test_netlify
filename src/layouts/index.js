3f0e2a941694344888049da49254e80a261a02ba
MSI\Shu <mathew.deal@369webcash.com>
2018-01-31 12:13:39 +0900

    Fixed index html class to allow fixed navbar space

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,9 @@ import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Gatsby + Netlify CMS">
+      <html className="has-navbar-fixed-top" />
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
   </div>
