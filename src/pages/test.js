1e5e7f3a31c5b506643dca5b0600ddc54fc47885
MSI\Shu <mathew.deal@369webcash.com>
2018-02-15 18:37:17 +0900

    簡単なテストページを追加（新規ページ作成の参考）

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default class TestPage extends React.Component {
   render() {
-    return <h1>This is a test page.</h1>;
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Hello World</h1>
+          <p className="subtitle">
+            This is a test page using <strong>Bulma</strong>.
+          </p>
+        </div>
+      </section>
+    );
   }
 }
