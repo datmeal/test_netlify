8a32110e126ff06400581e5763b1595c96d104af
MSI\Shu <mathew.deal@369webcash.com>
2018-02-15 18:54:43 +0900

    Fixed hero image resizing issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -180,8 +180,7 @@ export default class IndexPage extends React.Component {
               <Member alt="test" src="/img/t_hoshikawa_color_350-300x300.jpg" />
             </div>
             <h2 className="subtitle has-text-primary has-text-weight-bold is-size-5 has-text-centered">
-              新しい未来を私達と一緒に作りませんか？
-              {/* Future is not in front of us, it is inside of us. */}
+              {text.future}
             </h2>
             <div className="level">
               <div className="level-item">
